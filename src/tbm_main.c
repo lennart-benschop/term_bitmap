bd30f5d87ef02d48336c2c07a3c04922f50c45fd
Lennart Benschop <lennartb@xs4all.nl>
2026-08-10 21:18:59 +0200

    Fixed compile error

--- src/tbm_main.c
+++ src/tbm_main.c
@@ -73,7 +73,7 @@ tbm_new_screen(unsigned int width,
   bms->fgcolor = c - 1;
   bms->fgmode = DRAW_MODE_FG;
   bms->bgmode = DRAW_MODE_INVIS;
-  set_palette(bms,c);
+  set_palette(bms);
   return (void*)bms;
 }
 
